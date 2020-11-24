5c38ea3312eeeeb73f2736a5134d1bb2c68040de
Alexander Bae <jabae@vpn12-client-172-20-197-10.princeton.edu>
2020-11-24 13:54:07 +0900

    Dissemble tiff script

--- dissemble_tiff.py
+++ dissemble_tiff.py
@@ -5,6 +5,16 @@ Separate tiff stack into individual tiff files.
 
 import numpy as np
 import tifffile as tif
+import os
+
+import argparse
+
+parser = argparse.ArgumentParser()
+
+# IO
+parser.add_argument("tif_file")
+
+args = parser.parse_args()
 
 
 def dissemble(tif_file, output_dir):
@@ -15,4 +25,29 @@ def dissemble(tif_file, output_dir):
 	for i in range(n):
 		tile = stack[i,:,:]
 		tif.imwrite(output_dir+"/s{}.tif".format(i), np.flip(tile, axis=0))
+
+
+def get_dir(file_path):
+
+	m = ""
+	i = 0
+	while m != "/":
+
+		i += 1
+		m = file_path[-i]
+		
+
+	return file_path[:-i+1]
+
+
+if __name__ == "__main__":
+
+	img_file = args.tif_file
 	
+	img_dir = get_dir(img_file)
+	img_dir = img_dir + "tif"
+
+	if not os.path.isdir(img_dir):
+		os.makedirs(img_dir)
+
+dissemble(img_file, img_dir)
