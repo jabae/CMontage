559ff5906aeaf31f9590f9d1ea25cab66ea6d8f2
jabae <jabae@princeton.edu>
2021-01-09 21:08:42 +0900

    export code update

--- Trakem2/export_patch.py
+++ Trakem2/export_patch.py
@@ -1,3 +1,5 @@
+import os
+
 from ij import IJ, ImagePlus
 from ij.process import FloatProcessor
 from array import zeros
@@ -9,29 +11,57 @@ from ini.trakem2.display import Patch, Display
 from ij.io import FileSaver
 
 
-# Open patches
-project = Project.getProjects()[0]
-layerset = project.getRootLayerSet()
+proj_list = ["/data/research/se/celegans/dataset3/N2DA_1430-2/L1/",
+			"/data/research/se/celegans/dataset3/N2DA_1430-2/L2/",
+			"/data/research/se/celegans/dataset3/N2DA_1430-2/L3/",
+			"/data/research/se/celegans/dataset3/N2DA_1430-2/L4/",
+			"/data/research/se/celegans/dataset3/N2DA_1430-2/L5/",
+			"/data/research/se/celegans/dataset3/N2DA_1430-2/M1/",
+			"/data/research/se/celegans/dataset3/N2DA_1430-2/M2/",
+			"/data/research/se/celegans/dataset3/N2DA_1430-2/M3/",
+			"/data/research/se/celegans/dataset3/N2DA_1430-2/M4/",
+			"/data/research/se/celegans/dataset3/N2DA_1430-2/M5/",
+			"/data/research/se/celegans/dataset3/N2DA_1430-2/N1/",
+			"/data/research/se/celegans/dataset3/N2DA_1430-2/N2/"]
 
-front = Display.getFront(project)
 
-layer = layerset.getLayers()[0]
-tiles = layer.getDisplayables(Patch)
+for i in range(len(proj_list)):
 
-patches = layer.getDisplayables(Patch)
+	data_dir = proj_list[i]
+	proj_dir = os.path.join(data_dir, "montage_v1")
+	img_dir = os.path.join(data_dir, "transformed")
 
-fname_trans = "/Users/jabae/Documents/montage/test_data/transform.csv"
-fw = open(fname_trans, "w")
-for i in range(len(patches)):
+	if not os.path.isdir(img_dir):
+		os.makedirs(img_dir)
 
-	patch = patches[i]
-	ip = patch.createCoordinateTransformedImage()
-	imp = ImagePlus("", ip.target)
+	proj_name =os.path.join(proj_dir, "montage_v1.xml")
+	project = Project.openFSProject(proj_name)
+	layerset = project.getRootLayerSet()
 
-	box = ip.box
-	fw.write("{}, {}, {}, {}, {}, {}\n".format(patch.title, int(layer.getZ()), box.x, box.y, box.width, box.height))
+	layer_list = layerset.getLayers()
+	
+	fname_trans = os.path.join(data_dir, "transform.csv")
+	fw = open(fname_trans, "w")
+	for j in range(len(layer_list)):
 
-	#fname = "/Users/jabae/Documents/montage/test_data/transformed/" + patch.title
-	#FileSaver(imp).saveAsTiff(fname)
+		layer = layer_list[j]
+		tiles = layer.getDisplayables(Patch)
 
-fw.close()
+		patches = layer.getDisplayables(Patch)
+
+		
+		for k in range(len(patches)):
+
+			patch = patches[k]
+			ip = patch.createCoordinateTransformedImage()
+			imp = ImagePlus("", ip.target)
+
+			box = ip.box
+			fw.write("{}, {}, {}, {}, {}, {}\n".format(patch.title, int(layer.getZ()), box.x, box.y, box.width, box.height))
+
+			fname = os.path.join(img_dir, patch.title)
+			FileSaver(imp).saveAsTiff(fname)
+
+	front = Display.getFront()
+	front.close(project)
+	fw.close()
--- scripts/export_sections.py
+++ scripts/export_sections.py
@@ -28,32 +28,32 @@ def distance(p1, p2):
 	return np.sum((p1 - p2)**2, axis=1)**0.5
 
 
-def compute_penalty(img):
+# def compute_penalty(img):
 
-	x_size = img.shape[0]
-	y_size = img.shape[1]
-	x_range = np.arange(x_size)
-	y_range = np.arange(y_size)
-	coords = np.array(list(itertools.product(x_range, y_range)))
+# 	x_size = img.shape[0]
+# 	y_size = img.shape[1]
+# 	x_range = np.arange(x_size)
+# 	y_range = np.arange(y_size)
+# 	coords = np.array(list(itertools.product(x_range, y_range)))
 
-	center = np.array([np.median(coords[:,0]), np.median(coords[:,1])])
-	center = center.reshape((1,2))
+# 	center = np.array([np.median(coords[:,0]), np.median(coords[:,1])])
+# 	center = center.reshape((1,2))
 
-	p = np.sum((coords - center)**2, axis=1)**0.5
+# 	p = np.sum((coords - center)**2, axis=1)**0.5
 
-	return p.reshape((x_size, y_size))
+# 	return p.reshape((x_size, y_size))
 
 
-def penalty_tile_dist(img, x_off, y_off, w_tile, h_tile, w, h):
+def compute_penalty(img, x_off, y_off, w, h):
 
-	ctr = np.array([int(x_off + w_tile//2), int(y_off + h_tile//2)]).reshape((1,2))
+	ctr = np.array([int(x_off), int(y_off)]).reshape((1,2))
 
 	d = np.min([distance(ctr, np.array([0,0])),
 							distance(ctr, np.array([h,0])),
 							distance(ctr, np.array([0,w])),
 							distance(ctr, np.array([h,w]))])
 
-	d = 1 - d/((w/2**2+h/2**2)**0.5)/2
+	# d = 1 - d/((w/2**2+h/2**2)**0.5)/2
 	p = np.ones(img.shape)*d
 
 	return p
@@ -63,11 +63,33 @@ if __name__ == "__main__":
 
 	data_dir = args.dir
 	img_dir = os.path.join(data_dir, "transformed")
+	out_dir = os.path.join(data_dir, "export_010520")
+	# out_dir = "/data/research/se/celegans/dataset3/N2DA_1430-2/Merged/sections
 
-	f_trans= os.path.join(data_dir, "transform.csv")
+	if not os.path.isdir(out_dir):
+		os.makedirs(out_dir)
+
+	f_trans = os.path.join(data_dir, "transform.csv")
 	trans_df = pd.read_csv(f_trans, header=None)
 	img_list = trans_df[0]
 	n_img = img_list.shape[0]
+
+	img_loc = []
+	x_loc = []
+	y_loc = []
+	f_loc = open(os.path.join(data_dir, "tile_location.txt"), "r")
+	for l in f_loc.readlines():
+
+		elem = l.split(" ")
+		img_loc.append(elem[0])
+		x_loc.append(int(elem[1]))
+		y_loc.append(int(elem[2]))
+
+	img_loc = np.array(img_loc)
+	x_loc = np.array(x_loc)
+	y_loc = np.array(y_loc)
+
+	f_loc.close()
 
 	# X, Y offset
 	x_off_list = np.array(trans_df[2])
@@ -88,39 +110,50 @@ if __name__ == "__main__":
 
 	for z in z_list:
 
+		valid = trans_df[1]==z
+		img_layer = np.array(img_list[valid])
+		n_img_layer = img_layer.shape[0]
+
+		x_off_layer = x_off_list[valid]
+		y_off_layer = y_off_list[valid]
+		w_tile_layer = np.array(trans_df[4][valid])
+		h_tile_layer = np.array(trans_df[5][valid])
+
 		img_sect = np.zeros((h,w), dtype="uint8")
-		wt_sect = np.ones((h,w))*9999
+		wt_sect = np.zeros((h,w))
 		img_sect = img_sect.reshape((-1,))
 		wt_sect = wt_sect.reshape((-1,))
 
-		for i in range(n_img):
+		for i in range(n_img_layer):
 
-			fname = os.path.join(img_dir, img_list[i])
+			fname = os.path.join(img_dir, img_layer[i])
 			tile = tif.imread(fname)
 
-			x_off = x_off_list[i]
-			y_off = y_off_list[i]
-			w_tile = trans_df[4][i]
-			h_tile = trans_df[5][i]
+			x_off = x_off_layer[i]
+			y_off = y_off_layer[i]
+			w_tile = w_tile_layer[i]
+			h_tile = h_tile_layer[i]
 
-			wt_tile = compute_penalty(tile)
-			wt_tile[np.where(tile==0)] = 9999
+			tile_idx = np.where(img_loc==img_layer[i])[0]
+			wt_tile = compute_penalty(tile, y_loc[tile_idx], x_loc[tile_idx], w, h)
+			wt_tile[np.where(tile==0)] = 0
 
 			img_temp = np.zeros((h,w))
-			wt_temp = np.ones((h,w))*9999
+			wt_temp = np.zeros((h,w))
 
+			# print(y_off+h_tile, x_off+w_tile)
 			img_temp[y_off:y_off+h_tile,x_off:x_off+w_tile] = tile
 			wt_temp[y_off:y_off+h_tile,x_off:x_off+w_tile] = wt_tile
 			img_temp = img_temp.reshape((-1,))
 			wt_temp = wt_temp.reshape((-1,))
 
-			valid = wt_sect > wt_temp
+			valid = wt_sect < wt_temp
 			img_sect[valid] = img_temp[valid]
 			wt_sect[valid] = wt_temp[valid]
-			print(i)
-
+			# print(i)
 
 		img_sect = img_sect.reshape(h,w)
 
-		fname_out = os.path.join(data_dir, "z_{}.tif".format(z)) 
+		fname_out = os.path.join(out_dir, "z_{}.tif".format(z)) 
 		tif.imwrite(fname_out, img_sect)
+		print("z = {} exported".format(z))
